75ed631a902526346afbb49350b7dfba81aeeeba
1212x <153285185+1212x@users.noreply.github.com>
2024-05-25 15:00:19 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -15,23 +15,6 @@ async def ping(_, message):
     rm = await message.reply_text("...")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms"
-
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-CMD = ["/", "."]
-
-@Client.on_message(filters.command("alive", CMD))
-async def check_alive(_, message):
-    await message.reply_text("**You are very lucky 🤞 I am alive ❤️ Press /start to use me**")
-
-@Client.on_message(filters.command("ping", CMD))
-async def ping(_, message):
-    start_t = time.time()
-    rm = await message.reply_text("...")
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 @Client.on_message(filters.command("Channels", CMD))
