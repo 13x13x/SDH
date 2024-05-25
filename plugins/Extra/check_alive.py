b9df7fd619fec80b8a58ed4c8b783332c4c0f6a9
1212x <153285185+1212x@users.noreply.github.com>
2024-05-25 14:41:27 +0530

    Update check_alive.py

--- plugins/Extra/check_alive.py
+++ plugins/Extra/check_alive.py
@@ -5,11 +5,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 CMD = ["/", "."]
 
-@app.on_message(filters.command("alive", CMD))
+@Client.on_message(filters.command("alive", CMD))
 async def check_alive(_, message):
     await message.reply_text("**You are very lucky 🤞 I am alive ❤️ Press /start to use me**")
 
-@app.on_message(filters.command("ping", CMD))
+@Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("...")
@@ -17,7 +17,7 @@ async def ping(_, message):
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
-@app.on_message(filters.command("PIFChannels", CMD))
+@Client.on_message(filters.command("PIFChannels", CMD))
 async def PIFChannels(_, message):
     keyboard = [
         [
