9ff0c0bcd5ca536547dcd69cfb9280fcc3c76160
13x13x <153285185+13x13x@users.noreply.github.com>
2024-07-19 11:10:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "http://PIFbots.online:22/")
+URL = environ.get("URL", "http://188.166.54.180/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
