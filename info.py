d996d5775022e1642697d3b0de0c763a3b3be4e3
13x13x <153285185+13x13x@users.noreply.github.com>
2024-08-07 14:02:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,7 +97,7 @@ SHORTLINK_API = environ.get('SHORTLINK_API', '')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "80")
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", None)
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "pifbots.online:22/")
+URL = environ.get("URL", "pifbots.online")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
